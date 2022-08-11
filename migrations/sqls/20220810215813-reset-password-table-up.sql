6c2817aebac15f168838a35b17230f3ab0667d83
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-08-11 04:16:10 +0200

    update reset_password TABLE

--- migrations/sqls/20220810215813-reset-password-table-up.sql
+++ migrations/sqls/20220810215813-reset-password-table-up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE reset_password (
     id SERIAL PRIMARY KEY,
     code VARCHAR(6) NOT NULL,
-    created_at DATE timestamp NULL,
-    expire_at DATE timestamp NULL,
+    created_at timestamp NOT NULL,
+    expire_at timestamp NOT NULL,
     user_id bigint REFERENCES users(id) NOT NULL
 );
