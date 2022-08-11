2643395889c6bd972a899e0d181c724bf433a931
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-08-11 04:14:47 +0200

    update email_verification TABLE

--- migrations/sqls/20220810220603-email-verification-table-up.sql
+++ migrations/sqls/20220810220603-email-verification-table-up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE email_verification (
     id SERIAL PRIMARY KEY,
     code VARCHAR(6) NOT NULL,
-    created_at DATE NOT NULL,
-    expire_at DATE NOT NULL,
+    created_at timestamp NOT NULL,
+    expire_at timestamp NOT NULL,
     user_id bigint REFERENCES users(id) NOT NULL
 );
