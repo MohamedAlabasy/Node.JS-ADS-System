dcbb904bfe989eecf45284977c075d71b7a2cc49
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-08-12 04:43:07 +0200

    add multer

--- src/controllers/adsController.ts
+++ src/controllers/adsController.ts
@@ -8,22 +8,26 @@ const ads = new AdsModels()
 // #			                            create                                         #
 // #=======================================================================================#
 export const create = async (request: Request, response: Response, next: NextFunction) => {
-    await ads.create(request)
-        .then(adsData => {
-            response.json({
-                status: 1,
-                data: {
-                    id: adsData.id,
-                    ads: adsData.ads,
-                    views_number: adsData.views,
-                    device_type: adsData.device_type,
-                    ads_place: adsData.ads_place,
-                    start_date: adsData.start_date,
-                    end_date: adsData.end_date,
-                    user_id: adsData.user_id,
-                }
-            })
-        }).catch(error => {
-            next(error)
-        })
+    response.json({
+        status: 1,
+        Image: request.file?.filename
+    })
+    // await ads.create(request)
+    //     .then(adsData => {
+    //         response.json({
+    //             status: 1,
+    //             data: {
+    //                 id: adsData.id,
+    //                 ads: adsData.ads,
+    //                 views_number: adsData.views,
+    //                 device_type: adsData.device_type,
+    //                 ads_place: adsData.ads_place,
+    //                 start_date: adsData.start_date,
+    //                 end_date: adsData.end_date,
+    //                 user_id: adsData.user_id,
+    //             }
+    //         })
+    //     }).catch(error => {
+    //         next(error)
+    //     })
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import 'dotenv/config'
 import body_parser from 'body-parser';
 import cors from 'cors';
+import multer from 'multer';
+import path from 'path';
+
 
 import morganMiddleware from './middleware/morganMiddleware';
 import notFoundMiddleware from './middleware/notFoundMiddleware';
@@ -21,6 +24,36 @@ app.listen(process.env.PORT || 8888, () => {
 // #=======================================================================================#
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
+// #=======================================================================================#
+// #			                         for add images                                    #
+// #=======================================================================================#
+const storage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        callback(null, path.join(__dirname, 'Public/assets'))
+    },
+    filename: (request, file, callback) => {
+        callback(null, new Date().toUTCString().replace(/[: ]/g, '-') + '-' + file.originalname.replace(/ /g, '-'));
+    }
+});
+const fileFilter = (request: express.Request, file: any, callback: any) => {
+    if (// image extensions
+        file.mimetype == 'image/png' ||
+        file.mimetype == 'image/jpg' ||
+        file.mimetype == 'image/jpeg' ||
+        // video extensions
+        file.mimetype == 'video/gif'||
+        file.mimetype == 'video/mkv'||
+        file.mimetype == 'video/mp4') {
+        callback(null, true);
+    } else {
+        callback(null, false);
+    }
+}
+// to allowed front end to access my image
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(multer({ storage, fileFilter }).single('ads'));
+
 // #=======================================================================================#
 // #			                     add header or use cors                                #
 // #=======================================================================================#
